134b2afed70367192548d345a584db27fdd60d72
shivaconceptsolution <50239824+shivaconceptsolution@users.noreply.github.com>
2019-05-24 09:21:25 +0530

    Add files via upload

---
+++ Controllers/AjaxController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCExample.Models;
+namespace MVCExample.Controllers
+{
+    public class AjaxController : Controller
+    {
+        Database1Entities db = new Database1Entities();
+        // GET: Ajax
+        public ActionResult Index()
+        {
+            ViewBag.StuData = new SelectList(db.Students, "Rno", "Sname");
+            return View();
+        }
+        public ActionResult searchdemo(int id)
+        {
+            // var s = from c in db.Students where c.Sname.StartsWith(id) select c;
+            var s = from c in db.Students where c.Rno == id select c;
+            return View(s.ToList());
+        }
+    }
+}
